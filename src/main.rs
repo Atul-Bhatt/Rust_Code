3559d6e78869f94ba65c3fab9ecb3a3f45d77075
Atul Bhatt <atulbhatt61@gmail.com>
2023-02-09 13:23:24 +0530

    Added can_hold method to Rectangle

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,33 @@
 #[derive(Debug)]
 
 /*
-    printing rectangle in debug mode. You can't do it in normal mode because
-    your rectangle struct doesn't implement 'std::fmt::Display'
+    Implementing a can_hold() method for Rectangle struct
 */
-struct Rectangle(i32, i32);
+struct Rectangle {
+    width: i32,
+    height: i32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width >= rect.width && self.height >= rect.height
+    }
+}
 
 fn main() {
-    
-    let rect: Rectangle = Rectangle(10, 40);
+    let rect1 = Rectangle {
+        width: 10, 
+        height: 20,
+    };
+    let rect2 = Rectangle {
+        width: 30,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 20, 
+        height: 20,
+    };
 
-    println!("rect is {:#?}", rect);
+    println!("Can rect1 hold rect2: {}", rect1.can_hold(&rect2));
+    println!("Can rect2 hold rect1: {}", rect2.can_hold(&rect1));
 }
