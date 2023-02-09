30beaa999ac30d932cef60f2fc4c5aac55da8474
Atul Bhatt <atulbhatt61@gmail.com>
2023-02-09 12:23:39 +0530

    Area of rectangle using tuple struct

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,20 @@
 
-// Calculating the area of a rectangle using a struct
+// Calculating the area of a rectangle using a tuple struct
 
-struct Rectangle {
-    width: i32,
-    height: i32,
-}
+// Tuple struct implementation doesn't specify which one is width and which one is height
+struct Rectangle(i32, i32);
 
 fn main() {
     
-    let rect: Rectangle = build_rectangle(10, 25);
+    let rect: Rectangle = Rectangle(10, 40);
 
     println!("Area of the rectangle is: {}",
         calc_rec_area(rect)
     );
 }
 
-// This function takes input width and height and returns an instance of Rectangle Struct 
-
-fn build_rectangle(width: i32, height: i32) -> Rectangle {
-    Rectangle {
-        width,
-        height,
-    }
-}
-
 // This function takes input a Rectangle instance and returns the area of the rectangle
 
 fn calc_rec_area(rect: Rectangle) -> i32{
-    rect.width * rect.height
+    rect.0 * rect.1
 }
