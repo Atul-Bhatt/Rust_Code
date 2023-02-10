5b13be4123e280789bedb0a210c2b10fe97eef25
Atul Bhatt <atulbhatt61@gmail.com>
2023-02-10 15:54:22 +0530

    created math module.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
+mod math;
+use math::{Addition, Subtraction};
 
-#[derive(Debug)]
-
-/*
-    Implementing a can_hold() method for Rectangle struct
-*/
-struct Rectangle {
-    width: i32,
-    height: i32,
-}
-
-impl Rectangle {
-    fn can_hold(&self, rect: &Rectangle) -> bool {
-        self.width >= rect.width && self.height >= rect.height
-    }
-}
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 10, 
-        height: 20,
-    };
-    let rect2 = Rectangle {
-        width: 30,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 20, 
-        height: 20,
-    };
-
-    println!("Can rect1 hold rect2: {}", rect1.can_hold(&rect2));
-    println!("Can rect2 hold rect1: {}", rect2.can_hold(&rect1));
+    let x = 10;
+    let y = 20;
+    
+    println!("Sum is {}", Addition::add(x, y));
+    println!("Difference is {}", Subtraction::sub(x, y));
 }
---
+++ src/math.rs
@@ -0,0 +1,11 @@
+pub mod Addition {
+    pub fn add(x: i32, y: i32) -> i32{
+        x + y
+    }
+}
+pub mod Subtraction {
+    pub fn sub(x: i32, y: i32) -> i32{
+        x - y
+    }
+}
+
