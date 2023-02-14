6297a0f5552e0272ae447bdef46bce9193b24211
Atul Bhatt <atulbhatt61@gmail.com>
2023-02-14 12:51:37 +0530

    Config struct, config implements new function.

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,24 @@ fn main() {
     // use std::evn::args to take input from command line and collect to turn iterator into collection.
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_config(&args);
+    let config: Config = Config::new(&args);
 
     // Open the file given in filepath
-    let file_content = fs::read_to_string(file_path)
+    let file_content = fs::read_to_string(config.file_path)
             .expect("Cannot read file.");
     println!("{file_content}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config {query, file_path}
+    }
 }
