195f9c3d20c728ee9b050f8bf0c69794e89e2c77
Atul-Bhatt <atul.bhatt61@gmail.com>
2023-02-14 15:07:55 +0530

    new to build, return type Result.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 /*
     Creating grep like command line application
@@ -9,7 +10,10 @@ fn main() {
     // use std::evn::args to take input from command line and collect to turn iterator into collection.
     let args: Vec<String> = env::args().collect();
 
-    let config: Config = Config::new(&args);
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Reading Arguments Failed: {err}");
+        process::exit(1);
+    });
 
     // Open the file given in filepath
     let file_content = fs::read_to_string(config.file_path)
@@ -23,10 +27,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-    let file_path = args[2].clone();
+    fn build(args: &[String]) -> Result<Config, &'static str> {
 
-    Config {query, file_path}
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config {query, file_path})
     }
 }
