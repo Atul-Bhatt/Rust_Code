24a43e296b84fec305f5ebf7be66497d6ee4df26
Atul-Bhatt <atulbhatt61@gmail.com>
2023-02-14 16:34:59 +0530

    returning a Box error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 /*
     Creating grep like command line application
@@ -15,10 +16,18 @@ fn main() {
         process::exit(1);
     });
 
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Open the file given in filepath
-    let file_content = fs::read_to_string(config.file_path)
-            .expect("Cannot read file.");
+    let file_content = fs::read_to_string(config.file_path)?;
+
     println!("{file_content}");
+    Ok(())
 }
 
 struct Config {
