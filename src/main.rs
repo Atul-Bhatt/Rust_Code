d16dbe4d88189f9a3c4a2cc17625b7fffe6f4c03
Atul Bhatt <atulbhatt61@gmail.com>
2023-02-14 12:35:10 +0530

    Command line input, Reading a file, modularized with parse_config function

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,25 @@
-use std::io::{self, Read};
-use std::collections::HashMap;
-use std::collections::hash_map::Entry;
+use std::env;
+use std::fs;
 
 /*
-    Employee database using vectors and hashmaps
+    Creating grep like command line application
 */
 
 fn main() {
+    // use std::evn::args to take input from command line and collect to turn iterator into collection.
+    let args: Vec<String> = env::args().collect();
 
-    let mut database: HashMap<String, Vec<String>>= HashMap::new();
+    let (query, file_path) = parse_config(&args);
 
-    // Manually entering the departments with empty employee vector
+    // Open the file given in filepath
+    let file_content = fs::read_to_string(file_path)
+            .expect("Cannot read file.");
+    println!("{file_content}");
+}
 
-    database.insert(String::from("Engineering"), Vec::new());
-    database.insert(String::from("Sales"), Vec::new());
-    database.insert(String::from("Accounting"), Vec::new());
-    database.insert(String::from("HR"), Vec::new());
-    database.insert(String::from("Legal"), Vec::new());
-    
-    loop {
-        println!("Welcome to Employee Database");
-        println!("Please enter a command.");
-        println!("Here's an example -- Add Sheldon to Sales");
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
 
-        let mut command = String::new();
-
-        io::stdin()
-            .read_line(&mut command)
-            .expect("Cannot read string!");
-
-        let mut count = 0;
-        let mut department = String::new();
-        let mut employee_name = String::new();
-        
-        for word in command.split_whitespace() {
-            if count == 1 {
-                employee_name = word.to_string();
-            }
-            if count == 3 {
-                department = word.to_string();
-            }
-            count += 1;
-        }
-
-        match database.entry(department) {
-            Entry::Vacant(e) => { e.insert(vec![employee_name]); },
-            Entry::Occupied(mut e) => { e.get_mut().push(employee_name); },
-        }
-
-        println!("{:?}", database);
-    }
-    
+    (query, file_path)
 }
